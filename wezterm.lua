ea80830c3e4a69f4ab1eb774def31588985c8de6
Buster <60448635+SaintB2@users.noreply.github.com>
2023-02-04 05:29:31 +0000

    Add files via upload

---
+++ wezterm.lua
@@ -0,0 +1,29 @@
+term = "wezterm";
+
+local wezterm = require 'wezterm';
+
+local scheme = wezterm.get_builtin_color_schemes()["Gruvbox Dark"];
+scheme.background = "1d2021";
+
+wezterm.on('mux-is-process-stateful', function() return false end);
+
+return {
+	color_schemes = {
+		["Gruvbox Darker"] = scheme,
+	},
+	color_scheme = "Gruvbox Darker",
+	enable_tab_bar = false,
+	default_cursor_style = "SteadyBar",
+	term = "wezterm",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	freetype_load_target = "Normal",
+	check_for_updates = false,
+	animation_fps = 165,
+	front_end = "OpenGL",
+	prefer_egl = true,
+};
