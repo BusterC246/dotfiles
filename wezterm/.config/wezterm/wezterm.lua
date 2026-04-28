4275ad90dba58a58b23c27fbe54095620d0fd8b0
Buster Coden <busterc246@proton.me>
2026-04-28 10:10:49 -0400

    Fixed themes

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,14 +1,45 @@
 local wezterm = require 'wezterm';
 
--- local scheme = wezterm.get_builtin_color_schemes()["Gruvbox Dark"];
--- scheme.background = "1d2021";
+local scheme = wezterm.color.get_builtin_schemes()["Gruvbox Dark"];
+scheme = {
+	foreground = "#EBDBB2",
+	background = "#1D2021",
+	cursor_bg = "#EBDBB2",
+	cursor_fg = "#1D2021",
+	cursor_border = "#EBDBB2",
+	selection_fg = "#0a0e14",
+	selection_bg = "#cbccc6",
+	scrollbar_thumb = "#928374",
+	split = "#928374",
+	ansi = {
+		"#1D2021",
+		"#CC241D",
+		"#98971A",
+		"#D79921",
+		"#458588",
+		"#B16286",
+		"#689D6A",
+		"#A89984",
+	},
+	brights = {
+		"#928374",
+		"#FB4934",
+		"#B8BB26",
+		"#FADB2F",
+		"#83A598",
+		"#D3869B",
+		"#8EC07C",
+		"#EBDBB2",
+	},
+};
 
 wezterm.on('mux-is-process-stateful', function() return false end);
 
 return {
-	-- color_schemes = {
-		-- ["Gruvbox Darker"] = scheme,
-	-- },
+	color_schemes = {
+		["Gruvbox Dark"] = scheme,
+	},
+	color_scheme = "Gruvbox Dark",
 	enable_tab_bar = false,
 	default_cursor_style = "SteadyBar",
 	term = "xterm-color",
