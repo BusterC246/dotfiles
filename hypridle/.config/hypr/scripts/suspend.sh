a02aa4d5d1c662b318ddee0e303c276227f051af
Buster Coden <busterc246@proton.me>
2026-05-07 17:52:01 -0400

    Updated idle script.

--- hypridle/.config/hypr/scripts/suspend.sh
+++ hypridle/.config/hypr/scripts/suspend.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+
 MAX_LOAD_PER_CORE="${MAX_LOAD_PER_CORE:-0.50}"
 
 CORES="$(getconf _NPROCESSORS_ONLN 2>/dev/null || echo 1)"
-LOAD1="$(awk '{print $1}' /proc/loadavg)"
-
 THRESH="$(awk -v c="$CORES" -v p="$MAX_LOAD_PER_CORE" 'BEGIN{print c*p}')"
 
-if awk -v l="$LOAD1" -v t="$THRESH" 'BEGIN{exit !(l >= t)}'; then
-  exit 0
-fi
+while true; do
+  LOAD1="$(awk '{print $1}' /proc/loadavg)"
 
-loginctl suspend
+  if awk -v l="$LOAD1" -v t="$THRESH" 'BEGIN{exit !(l >= t)}'; then
+    sleep 60
+  else
+    loginctl suspend
+  fi
+done
